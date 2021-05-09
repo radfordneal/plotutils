06277d8ece2cc2aaafc9566bcac379c936cdb596
Radford Neal <radfordneal@gmail.com>
2021-05-09 17:37:37 -0400

    allow selection of only one method to run in xsum-time

--- xsum-time.c
+++ xsum-time.c
@@ -28,11 +28,15 @@
 
    Run with a command of the form:
 
-       xsum-time N M R [ data ... ] [ "inf" ] [ "perm" ]
+       xsum-time [ method ] N M R [ data ... ] [ "inf" ] [ "perm" ]
 
    Here, N is the size of the vectors that are summed, M is the number
-   of such vectors, and R is the number of times these M vectors of size
-   N are summed, by each method.
+   of such vectors, and R is the number of times these M vectors of
+   size N are summed, by each method.  All available methods are run,
+   unless a method is specified as the first argument, in which case
+   only it is run.  Possible methods are "sacc", "lacc", "iFast",
+   "OnlineExact", "float128", "double", and "udouble", some of which
+   may not be available depending on how xsum-time was compiled.
 
    After running these tests for summing vector elements, the sum of the
    squares is also computed, and then the dot product of each vector
@@ -100,24 +104,35 @@ int main (int argc, char **argv)
   int i, j, k;
   int ndata, n;
   char **data;
+  char *method;
   int perm, inf;
+
+  method = 0;
+  if (argc>0 && (argv[1][0]>='a' && argv[1][0]<='z'
+                  || argv[1][0]>='A' && argv[1][0]<='Z'))
+  { method = argv[1];
+    argv += 1;
+    argc -= 1;
+  }
 
   perm = 0;
   if (argc>0 && strcmp(argv[argc-1],"perm")==0)
   { perm = 1;
     argc -= 1;
   }
+
   inf = 0;
   if (argc>0 && strcmp(argv[argc-1],"inf")==0)
   { inf = 1;
     argc -= 1;
   }
+
   ndata = argc-4;
 
   if (argc<4 || (N=atoi(argv[1]))<1 || ndata>N
              || (M=atoi(argv[2]))<1 || (R=atoi(argv[3]))<1)
   { fprintf(stderr,
-            "Usage: xsum-time N M R [ data ... ] [ \"inf\" ] [ \"perm\" ] \n");
+  "Usage: xsum-time [ method ] N M R [ data ... ] [ \"inf\" ] [ \"perm\" ] \n");
     exit(1);
   }
 
@@ -186,98 +201,114 @@ int main (int argc, char **argv)
 
   printf("\nVECTOR SUM\n\n");
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { xsum_small_init(&sacc);
-      xsum_small_addv(&sacc,a+k*N,N);
-      result_s = xsum_small_round(&sacc);
+  if (method==0 || strcmp(method,"sacc")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { xsum_small_init(&sacc);
+        xsum_small_addv(&sacc,a+k*N,N);
+        result_s = xsum_small_round(&sacc);
+      }
     }
+    END_CLOCK;
+    printf("Small accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_s, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Small accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_s, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { xsum_large_init(&lacc);
-      xsum_large_addv(&lacc,a+k*N,N);
-      result_l = xsum_large_round(&lacc);
+  if (method==0 || strcmp(method,"lacc")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { xsum_large_init(&lacc);
+        xsum_large_addv(&lacc,a+k*N,N);
+        result_l = xsum_large_round(&lacc);
+      }
     }
+    END_CLOCK;
+    printf("Large accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_l, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Large accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_l, clock_dur, clock_dur*1e9/R/N/M);
 
 # ifdef ZHU
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { memcpy (ai, a+k*N, N * sizeof *ai);
-      result_iFast = mysum.iFastSum(ai-1,N);
+  if (method==0 || strcmp(method,"iFast")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { memcpy (ai, a+k*N, N * sizeof *ai);
+        result_iFast = mysum.iFastSum(ai-1,N);
+      }
     }
+    END_CLOCK;
+    printf("iFastSum result:    %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_iFast, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("iFastSum result:    %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_iFast, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_Online = mysum.OnlineExactSum(a+k*N-1,N);
+  if (method==0 || strcmp(method,"OnlineExact")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_Online = mysum.OnlineExactSum(a+k*N-1,N);
+      }
     }
+    END_CLOCK;
+    printf("OnlineExact result: %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_Online, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("OnlineExact result: %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_Online, clock_dur, clock_dur*1e9/R/N/M);
 
 # endif
 
 # ifdef FLOAT128
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_128 = xsum_sum_float128(a+k*N,N);
+  if (method==0 || strcmp(method,"float128")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_128 = xsum_sum_float128(a+k*N,N);
+      }
     }
+    END_CLOCK;
+    printf("Float 128 result:   %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_128, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Float 128 result:   %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_128, clock_dur, clock_dur*1e9/R/N/M);
 
 # endif
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_k = xsum_sum_kahan(a+k*N,N);
+  if (method==0 || strcmp(method,"Kahan")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_k = xsum_sum_kahan(a+k*N,N);
+      }
     }
+    END_CLOCK;
+    printf("Kahan sum result:   %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_k, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Kahan sum result:   %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_k, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_d = xsum_sum_double(a+k*N,N);
+  if (method==0 || strcmp(method,"double")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_d = xsum_sum_double(a+k*N,N);
+      }
     }
+    END_CLOCK;
+    printf("Double result:      %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_d, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Double result:      %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_d, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_dno = xsum_sum_double_not_ordered(a+k*N,N);
+  if (method==0 || strcmp(method,"udouble")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_dno = xsum_sum_double_not_ordered(a+k*N,N);
+      }
     }
+    END_CLOCK;
+    printf("Double not ordered: %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_dno, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Double not ordered: %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_dno, clock_dur, clock_dur*1e9/R/N/M);
 
   if (ndata <= 2)
   { double correct;
@@ -289,206 +320,240 @@ int main (int argc, char **argv)
     { correct = 0.0;
       for (i = 0; i < ndata; i++) correct += atof(data[i]);
     }
-    if (different(result_s,correct))
+    if ((method==0 || strcmp(method,"sacc")==0) && different(result_s,correct))
     { printf("RESULT USING SMALL ACCUMULATOR IS WRONG\n");
     }
-    if (different(result_l,correct))
+    if ((method==0 || strcmp(method,"lacc")==0) && different(result_l,correct))
     { printf("RESULT USING LARGE ACCUMULATOR IS WRONG\n");
     }
   }
 
-  if (result_s != (double) xsum_small_round(&sacc))
+  if ((method==0 || strcmp(method,"sacc")==0) 
+        && result_s != (double) xsum_small_round(&sacc))
   { printf ("RESULT DIFFERS AFTER ROUNDING SMALL ACCUMULATOR TWICE\n");
   }
 
-  if (result_l != (double) xsum_large_round(&lacc))
+  if ((method==0 || strcmp(method,"lacc")==0) 
+        &&result_l != (double) xsum_large_round(&lacc))
   { printf ("RESULT DIFFERS AFTER ROUNDING LARGE ACCUMULATOR TWICE\n");
   }
 
-  if (result_s != result_l)
+  if (method==0 && result_s != result_l)
   { printf("RESULTS DIFFER FOR SMALL AND LARGE ACCUMULATORS\n");
   }
 
 # ifdef ZHU
 
-  if (result_iFast != result_l)
+  if (method==0 && result_iFast != result_l)
   { printf("RESULTS DIFFER FOR IFASTSUM AND LARGE ACCUMULATOR\n");
   }
 
-  if (result_Online != result_l)
+  if (method==0 && result_Online != result_l)
   { printf("RESULTS DIFFER FOR ONLINEEXACT AND LARGE ACCUMULATOR\n");
   }
 
 # endif
 
-  if (N <= 2 && result_s != result_d)
+  if (method==0 && N <= 2 && result_s != result_d)
   { printf("RESULTS DIFFER FOR DOUBLE AND SMALL ACCUMULATOR\n");
   }
 
-  if (N <= 2 && result_l != result_d)
+  if (method==0 && N <= 2 && result_l != result_d)
   { printf("RESULTS DIFFER FOR DOUBLE AND LARGE ACCUMULATOR\n");
   }
 
   printf("\n");
-  if (!perm)
+  if ((method==0 || strcmp(method,"sacc")==0) && !perm)
   { xsum_small_init(&sacc); /* Get used counts half-way, before cancels to 0 */
     xsum_small_addv(&sacc,a,N/2);
     used_small = xsum_small_chunks_used(&sacc);
     printf("Small accumulator chunks used: %d\n", used_small);
   }
 
-  used_large = xsum_large_chunks_used(&lacc);
-  printf("Large accumulator chunks used: %d\n", used_large);
+  if (method==0 || strcmp(method,"lacc")==0)
+  { used_large = xsum_large_chunks_used(&lacc);
+    printf("Large accumulator chunks used: %d\n", used_large);
+  }
 
   printf("\nVECTOR NORM\n\n");
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { xsum_small_init(&sacc);
-      xsum_small_add_sqnorm(&sacc,a+k*N,N);
-      result_s = xsum_small_round(&sacc);
+  if (method==0 || strcmp(method,"sacc")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { xsum_small_init(&sacc);
+        xsum_small_add_sqnorm(&sacc,a+k*N,N);
+        result_s = xsum_small_round(&sacc);
+      }
     }
+    END_CLOCK;
+    printf("Small accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_s, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Small accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_s, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { xsum_large_init(&lacc);
-      xsum_large_add_sqnorm(&lacc,a+k*N,N);
-      result_l = xsum_large_round(&lacc);
+  if (method==0 || strcmp(method,"lacc")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { xsum_large_init(&lacc);
+        xsum_large_add_sqnorm(&lacc,a+k*N,N);
+        result_l = xsum_large_round(&lacc);
+      }
     }
+    END_CLOCK;
+    printf("Large accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_l, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Large accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_l, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_d = xsum_sqnorm_double(a+k*N,N);
+  if (method==0 || strcmp(method,"double")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_d = xsum_sqnorm_double(a+k*N,N);
+      }
     }
+    END_CLOCK;
+    printf("Double result:      %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_d, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Double result:      %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_d, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_dno = xsum_sqnorm_double_not_ordered(a+k*N,N);
+  if (method==0 || strcmp(method,"udouble")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_dno = xsum_sqnorm_double_not_ordered(a+k*N,N);
+      }
     }
+    END_CLOCK;
+    printf("Double not ordered: %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_dno, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Double not ordered: %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_dno, clock_dur, clock_dur*1e9/R/N/M);
 
-  if (result_s != (double) xsum_small_round(&sacc))
+  if ((method==0 || strcmp(method,"sacc")==0)
+        && result_s != (double) xsum_small_round(&sacc))
   { printf ("RESULT DIFFERS AFTER ROUNDING SMALL ACCUMULATOR TWICE\n");
   }
 
-  if (result_l != (double) xsum_large_round(&lacc))
+  if ((method==0 || strcmp(method,"lacc")==0)
+        && result_l != (double) xsum_large_round(&lacc))
   { printf ("RESULT DIFFERS AFTER ROUNDING LARGE ACCUMULATOR TWICE\n");
   }
 
-  if (result_s != result_l)
+  if (method==0 && result_s != result_l)
   { printf("RESULTS DIFFER FOR SMALL AND LARGE ACCUMULATORS\n");
   }
 
-  if (N <= 2 && result_s != result_d)
+  if (method==0 && N <= 2 && result_s != result_d)
   { printf("RESULTS DIFFER FOR DOUBLE AND SMALL ACCUMULATOR\n");
   }
 
-  if (N <= 2 && result_l != result_d)
+  if (method==0 && N <= 2 && result_l != result_d)
   { printf("RESULTS DIFFER FOR DOUBLE AND LARGE ACCUMULATOR\n");
   }
 
-  xsum_small_init(&sacc);
-  xsum_small_add_sqnorm(&sacc,a,N);
-  used_small = xsum_small_chunks_used(&sacc);
-  used_large = xsum_large_chunks_used(&lacc);
-
   printf("\n");
-  printf("Small accumulator chunks used: %d\n", used_small);
-  printf("Large accumulator chunks used: %d\n", used_large);
+
+  if (method==0 || strcmp(method,"sacc")==0)
+  { xsum_small_init(&sacc);
+    xsum_small_add_sqnorm(&sacc,a,N);
+    used_small = xsum_small_chunks_used(&sacc);
+    printf("Small accumulator chunks used: %d\n", used_small);
+  }
+
+  if (method==0 || strcmp(method,"lacc")==0)
+  { used_large = xsum_large_chunks_used(&lacc);
+    printf("Large accumulator chunks used: %d\n", used_large);
+  }
 
   printf("\nVECTOR DOT PRODUCT\n\n");
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { xsum_small_init(&sacc);
-      xsum_small_add_dot(&sacc,a+k*N,a2+k*N,N);
-      result_s = xsum_small_round(&sacc);
+  if (method==0 || strcmp(method,"sacc")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { xsum_small_init(&sacc);
+        xsum_small_add_dot(&sacc,a+k*N,a2+k*N,N);
+        result_s = xsum_small_round(&sacc);
+      }
     }
+    END_CLOCK;
+    printf("Small accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_s, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Small accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_s, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { xsum_large_init(&lacc);
-      xsum_large_add_dot(&lacc,a+k*N,a2+k*N,N);
-      result_l = xsum_large_round(&lacc);
+  if (method==0 || strcmp(method,"lacc")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { xsum_large_init(&lacc);
+        xsum_large_add_dot(&lacc,a+k*N,a2+k*N,N);
+        result_l = xsum_large_round(&lacc);
+      }
     }
+    END_CLOCK;
+    printf("Large accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_l, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Large accumulator:  %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_l, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_d = xsum_dot_double(a+k*N,a2+k*N,N);
+  if (method==0 || strcmp(method,"double")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_d = xsum_dot_double(a+k*N,a2+k*N,N);
+      }
     }
+    END_CLOCK;
+    printf("Double result:      %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_d, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Double result:      %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_d, clock_dur, clock_dur*1e9/R/N/M);
 
-  START_CLOCK;
-  for (j = 0; j < R; j++)
-  { for (k = 0; k < M; k++)
-    { result_dno = xsum_dot_double_not_ordered(a+k*N,a2+k*N,N);
+  if (method==0 || strcmp(method,"udouble")==0)
+  { START_CLOCK;
+    for (j = 0; j < R; j++)
+    { for (k = 0; k < M; k++)
+      { result_dno = xsum_dot_double_not_ordered(a+k*N,a2+k*N,N);
+      }
     }
+    END_CLOCK;
+    printf("Double not ordered: %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
+           result_dno, clock_dur, clock_dur*1e9/R/N/M);
   }
-  END_CLOCK;
-  printf("Double not ordered: %+.16le  time: %7.3lf s, %7.2lf ns/term\n",
-         result_dno, clock_dur, clock_dur*1e9/R/N/M);
 
-  if (result_s != (double) xsum_small_round(&sacc))
+  if ((method==0 || strcmp(method,"sacc")==0)
+        && result_s != (double) xsum_small_round(&sacc))
   { printf ("RESULT DIFFERS AFTER ROUNDING SMALL ACCUMULATOR TWICE\n");
   }
 
-  if (result_l != (double) xsum_large_round(&lacc))
+  if ((method==0 || strcmp(method,"lacc")==0)
+        && result_l != (double) xsum_large_round(&lacc))
   { printf ("RESULT DIFFERS AFTER ROUNDING LARGE ACCUMULATOR TWICE\n");
   }
 
-  if (result_s != result_l)
+  if (method==0 && result_s != result_l)
   { printf("RESULTS DIFFER FOR SMALL AND LARGE ACCUMULATORS\n");
   }
 
-  if (N <= 2 && result_s != result_d)
+  if (method==0 && N <= 2 && result_s != result_d)
   { printf("RESULTS DIFFER FOR DOUBLE AND SMALL ACCUMULATOR\n");
   }
 
-  if (N <= 2 && result_l != result_d)
+  if (method==0 && N <= 2 && result_l != result_d)
   { printf("RESULTS DIFFER FOR DOUBLE AND LARGE ACCUMULATOR\n");
   }
 
-  xsum_small_init(&sacc);
-  xsum_small_add_dot(&sacc,a,a2,N);
-  used_small = xsum_small_chunks_used(&sacc);
-  used_large = xsum_large_chunks_used(&lacc);
-
   printf("\n");
-  printf("Small accumulator chunks used: %d\n", used_small);
-  printf("Large accumulator chunks used: %d\n", used_large);
+
+  if (method==0 || strcmp(method,"sacc")==0)
+  { xsum_small_init(&sacc);
+    xsum_small_add_dot(&sacc,a,a2,N);
+    used_small = xsum_small_chunks_used(&sacc);
+    printf("Small accumulator chunks used: %d\n", used_small);
+  }
+
+  if (method==0 || strcmp(method,"lacc")==0)
+  { used_large = xsum_large_chunks_used(&lacc);
+    printf("Large accumulator chunks used: %d\n", used_large);
+  }
 
   printf("\n");
 
