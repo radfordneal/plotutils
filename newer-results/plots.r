c556da73d086f28eef47f4e5cf4fd5aaf9fa2eb0
Radford Neal <radfordneal@gmail.com>
2021-05-13 14:47:21 -0400

    Better plotting scheme.  Now show results with all compilers (in light colour), as well as "best" choice (in darker colour).

--- newer-results/plots.r
+++ newer-results/plots.r
@@ -29,25 +29,28 @@ plot_key <- function ()
   dev.off()
 }
 
-plot_times <- function (y, what, cache)
+plot_times <- function (y, what, cache, 
+                        col=c("red","red","blue","blue","black","black"),
+                        lty=c(1,2,1,2,1,2), add=FALSE)
 {
-  pdf(paste0("./",what,".pdf"),height=4.28,width=5,pointsize=11)
-  par(mar=c(4.4,3.3,0.2,0.2),mgp=c(2.0,0.6,0),lab=c(7,7,7),cex.lab=1.5)
-  col=c("red","red","blue","blue","black","black")
-  lty=c(1,2,1,2,1,2)
-  plot(1,type="n",xlab="number of terms",ylab="nanoseconds per term",
-       xlim=c(1,7),ylim=c(-2.1,6.5),xaxt="n",yaxt="n")
-  abline(h=seq(-1,7),col="gray")
   logy <- log(y,2)
+
+  if (!add)
+  { pdf(paste0("./",what,".pdf"),height=4.28,width=5,pointsize=11)
+    par(mar=c(4.4,3.3,0.2,0.2),mgp=c(2.0,0.6,0),lab=c(7,7,7),cex.lab=1.5)
+    plot(1,type="n",xlab="number of terms",ylab="nanoseconds per term",
+         xlim=c(1,7),ylim=c(-2.1,6.5),xaxt="n",yaxt="n")
+    abline(h=seq(-1,7),col="gray")
+    mtext (2^((-2):6), side=2, line=0.3, at=(-2):6, cex=0.85)
+    mtext (1:7, side=1, line=0.05, at=(1:7)+0.1, cex=0.7)
+    mtext (rep(10,7), side=1, line=0.55, at=(1:7)-0.04, cex=0.85)
+    abline(col="gray",v=log10(cache))
+  }
+
   for (j in 1:ncol(y))
   { lines (1:7, logy[,j], col=col[j], lty=lty[j])
     points (1:7, logy[,j], col=col[j], pch=20)
   }
-  mtext (2^((-2):6), side=2, line=0.3, at=(-2):6, cex=0.85)
-  mtext (1:7, side=1, line=0.05, at=(1:7)+0.1, cex=0.7)
-  mtext (rep(10,7), side=1, line=0.55, at=(1:7)-0.04, cex=0.85)
-  abline(col="gray",v=log10(cache))
-  dev.off()
 }
 
 read_times <- function (file)
@@ -75,6 +78,27 @@ read_times <- function (file)
   m
 }
 
+find_min_times <- function (...,
+                    ref = c(sacc=3, lacc=5, ifast=3, online=5, dbl=4, udbl=4))
+{
+  times <- list(...)
+  min_times <- times[[1]]
+
+  for (nxt in times[-1])
+  { for (k in 1:dim(nxt)[3])
+    { w <- !is.na(nxt[1,,k])
+      dn <- dimnames(nxt)[[2]]
+      for (j in (1:dim(nxt)[2])[w])
+      { if (nxt[ref[dn[j]],j,k] < min_times[ref[dn[j]],j,k])
+        { min_times[,j,k] <- nxt[,j,k]
+        }
+      }
+    }
+  }
+
+  min_times
+}
+
 
 # CREATE PLOTS FOR VARIOUS NEWER SYSTEMS.
 
@@ -88,27 +112,44 @@ cache = c(32*1024, 512*1024, 16*1024*1024)
 thinkstation_gcc_10_2_0 <- read_times ("results-thinkstation-gcc-10.2.0")
 thinkstation_clang_10_0_0 <- read_times ("results-thinkstation-clang-10.0.0")
 
-thinkstation <- thinkstation_clang_10_0_0
-thinkstation[,"dbl",] <- thinkstation_gcc_10_2_0[,"dbl",]
-thinkstation[,"udbl",] <- thinkstation_gcc_10_2_0[,"udbl",]
-thinkstation[,"lacc",] <- thinkstation_gcc_10_2_0[,"lacc",]
+thinkstation <- find_min_times (thinkstation_gcc_10_2_0,
+                                thinkstation_clang_10_0_0)
 
-plot_times(thinkstation[,,"sum"],  "thinkstation-sum",  cache/8)
-plot_times(thinkstation[,,"norm"], "thinkstation-norm", cache/8)
-plot_times(thinkstation[,,"dot"],  "thinkstation-dot",  cache/16)
+for (task in c("sum","norm","dot"))
+{
+  plot_times (thinkstation[,,task],  
+              paste0("thinkstation-",task), 
+              cache / c(sum=8,norm=8,dot=16)[task])
+  for (dat in list (thinkstation_gcc_10_2_0,
+                    thinkstation_clang_10_0_0))
+  { plot_times(dat[,,task], add=TRUE,
+               col=c("pink","pink","lightblue","lightblue","gray","gray"))
+  }
+  plot_times(thinkstation[,,task], add=TRUE)
+  dev.off()
+}
 
 thinkstation_gcc_10_2_0_no_opt <- 
   read_times ("results-thinkstation-gcc-10.2.0-no-opt")
 thinkstation_clang_10_0_0_no_opt <- 
   read_times ("results-thinkstation-clang-10.0.0-no-opt")
 
-thinkstation_no_opt <- thinkstation_clang_10_0_0_no_opt
-thinkstation_no_opt[,"dbl",] <- thinkstation_gcc_10_2_0_no_opt[,"dbl",]
-thinkstation_no_opt[,"udbl",] <- thinkstation_gcc_10_2_0_no_opt[,"udbl",]
+thinkstation_no_opt <- find_min_times (thinkstation_gcc_10_2_0_no_opt,
+                                       thinkstation_clang_10_0_0_no_opt)
 
-plot_times(thinkstation_no_opt[,,"sum"],  "thinkstation-no-opt-sum",  cache/8)
-plot_times(thinkstation_no_opt[,,"norm"], "thinkstation-no-opt-norm", cache/8)
-plot_times(thinkstation_no_opt[,,"dot"],  "thinkstation-no-opt-dot",  cache/16)
+for (task in c("sum","norm","dot"))
+{
+  plot_times (thinkstation_no_opt[,,task],  
+              paste0("thinkstation-no-opt-",task), 
+              cache / c(sum=8,norm=8,dot=16)[task])
+  for (dat in list (thinkstation_gcc_10_2_0_no_opt,
+                    thinkstation_clang_10_0_0_no_opt))
+  { plot_times(dat[,,task], add=TRUE,
+               col=c("pink","pink","lightblue","lightblue","gray","gray"))
+  }
+  plot_times(thinkstation_no_opt[,,task], add=TRUE)
+  dev.off()
+}
 
 
 # Intel i7-3520M.
@@ -118,25 +159,44 @@ cache = c(32*1024, 256*1024)
 macbookpro2_gcc_10_2_0 <- read_times ("results-macbookpro2-gcc-10.2.0")
 macbookpro2_clang_9_0_0 <- read_times ("results-macbookpro2-clang-9.0.0")
 
-macbookpro2 <- macbookpro2_clang_9_0_0
-macbookpro2[,"sacc",] <- macbookpro2_gcc_10_2_0[,"sacc",]
-macbookpro2[,"ifast",] <- macbookpro2_gcc_10_2_0[,"ifast",]
-macbookpro2[,"udbl",] <- macbookpro2_gcc_10_2_0[,"udbl",]
-macbookpro2[,"lacc",] <- macbookpro2_gcc_10_2_0[,"lacc",]
+macbookpro2_gcc_10_2_0 <- read_times ("results-macbookpro2-gcc-10.2.0")
+macbookpro2_clang_10_0_0 <- read_times ("results-macbookpro2-clang-9.0.0")
 
-plot_times(macbookpro2[,,"sum"],  "macbookpro2-sum",  cache/8)
-plot_times(macbookpro2[,,"norm"], "macbookpro2-norm", cache/8)
-plot_times(macbookpro2[,,"dot"],  "macbookpro2-dot",  cache/16)
+macbookpro2 <- find_min_times (macbookpro2_gcc_10_2_0,
+                               macbookpro2_clang_10_0_0)
 
-macbookpro2_no_opt_gcc_10_2_0 <- 
+for (task in c("sum","norm","dot"))
+{
+  plot_times (macbookpro2[,,task],  
+              paste0("macbookpro2-",task), 
+              cache / c(sum=8,norm=8,dot=16)[task])
+  for (dat in list (macbookpro2_gcc_10_2_0,
+                    macbookpro2_clang_10_0_0))
+  { plot_times(dat[,,task], add=TRUE,
+               col=c("pink","pink","lightblue","lightblue","gray","gray"))
+  }
+  plot_times(macbookpro2[,,task], add=TRUE)
+  dev.off()
+}
+
+macbookpro2_gcc_10_2_0_no_opt <- 
   read_times ("results-macbookpro2-gcc-10.2.0-no-opt")
-macbookpro2_no_opt_clang_9_0_0 <- 
+macbookpro2_clang_10_0_0_no_opt <- 
   read_times ("results-macbookpro2-clang-9.0.0-no-opt")
 
-macbookpro2_no_opt <- macbookpro2_no_opt_clang_9_0_0
-macbookpro2_no_opt[,"sacc",] <- macbookpro2_no_opt_gcc_10_2_0[,"sacc",]
-macbookpro2_no_opt[,"dbl",] <- macbookpro2_no_opt_gcc_10_2_0[,"dbl",]
+macbookpro2_no_opt <- find_min_times (macbookpro2_gcc_10_2_0_no_opt,
+                                      macbookpro2_clang_10_0_0_no_opt)
 
-plot_times(macbookpro2_no_opt[,,"sum"],  "macbookpro2_no_opt-sum",  cache/8)
-plot_times(macbookpro2_no_opt[,,"norm"], "macbookpro2_no_opt-norm", cache/8)
-plot_times(macbookpro2_no_opt[,,"dot"],  "macbookpro2_no_opt-dot",  cache/16)
+for (task in c("sum","norm","dot"))
+{
+  plot_times (macbookpro2_no_opt[,,task],  
+              paste0("macbookpro2-no-opt-",task), 
+              cache / c(sum=8,norm=8,dot=16)[task])
+  for (dat in list (macbookpro2_gcc_10_2_0_no_opt,
+                    macbookpro2_clang_10_0_0_no_opt))
+  { plot_times(dat[,,task], add=TRUE,
+               col=c("pink","pink","lightblue","lightblue","gray","gray"))
+  }
+  plot_times(macbookpro2_no_opt[,,task], add=TRUE)
+  dev.off()
+}
